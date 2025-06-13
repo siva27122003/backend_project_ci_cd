87c4a634df7cae4b1c4c63368304f3a65ca82f19
siva27122003 <sivasankar27122003@gmail.com>
2025-06-13 11:40:25 +0530

    added docker-compe.yml file for containerize both go app and sql

--- Config/db.go
+++ Config/db.go
@@ -13,28 +13,35 @@ import (
 
 var DB *gorm.DB
 
-func initconfig() {
+func DbConnect() {
 	env := os.Getenv("APP_ENV")
 	fmt.Println("Env is ", env)
+
+	var dsn string
+
 	if env == "docker" {
-		viper.SetConfigFile("Config/config.docker.yaml")
+		// Get values from environment variables
+		user := os.Getenv("DB_USER")
+		password := os.Getenv("DB_PASSWORD")
+		host := os.Getenv("DB_HOST")
+		port := os.Getenv("DB_PORT")
+		name := os.Getenv("DB_NAME")
+
+		// Build DSN dynamically
+		dsn = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true&loc=Local", user, password, host, port, name)
 	} else {
+		// Use local config file
 		viper.SetConfigFile("Config/config.yaml")
+		err := viper.ReadInConfig()
+		if err != nil {
+			log.Fatalf("Error reading config file: %s", err)
+		}
+		dsn = viper.GetString("dsn")
 	}
 
-	err := viper.ReadInConfig()
-	if err != nil {
-		log.Fatalf("Error reading config file: %s", err)
-	}
-}
-
-func DbConnect() {
-	initconfig()
-
-	dsn := viper.GetString("dsn")
 	fmt.Println("Dsn is ", dsn)
 	if dsn == "" {
-		log.Println("Config file doesn't have dsn...")
+		log.Fatal("DSN is empty! Check your config or environment variables.")
 	}
 
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
@@ -44,23 +51,7 @@ func DbConnect() {
 	fmt.Println("DB connection is successful...")
 
 	// Auto-migrate your models
-	err = db.AutoMigrate(&model.User{})
-	if err != nil {
-		log.Fatalf("Migration failed..%v", err)
-	}
-	err = db.AutoMigrate(&model.Farmer{})
-	if err != nil {
-		log.Fatalf("Migration failed..%v", err)
-	}
-	err = db.AutoMigrate(&model.Category{})
-	if err != nil {
-		log.Fatalf("Migration failed..%v", err)
-	}
-	err = db.AutoMigrate(&model.Commodity{})
-	if err != nil {
-		log.Fatalf("Migration failed..%v", err)
-	}
-	err = db.AutoMigrate(&model.Bidding{})
+	err = db.AutoMigrate(&model.User{}, &model.Farmer{}, &model.Category{}, &model.Commodity{}, &model.Bidding{})
 	if err != nil {
 		log.Fatalf("Migration failed..%v", err)
 	}
